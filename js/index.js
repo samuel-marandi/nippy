862c72f701b11abbfdb560e3fa76d7b6f6d88613
Samuel Marandi <samuel.marandi@evivehealth.com>
2018-09-24 15:49:56 +0530

    removed unneccessary files

--- js/index.js
+++ js/index.js
@@ -18,18 +18,6 @@ function handleRadioChange(event) {
   }
 }
 
-// function handleDestinationURLInputChange(event) {
-  
-//   let destination = destinationUrlInput.value;
-  
-//   if (destination.length === 0) {
-//     button.setAttribute('disabled', true);
-//   } else {
-//     button.setAttribute('disabled', false);
-//   }
-// }
-
-
 /**
  * Event handler for submit button
  * @param {*} event 
@@ -79,79 +67,3 @@ button.addEventListener('click', handleSubmitClick);
 radioButtonDev.addEventListener('change', handleRadioChange);
 radioButtonLocal.addEventListener('change', handleRadioChange);
 radioButtonCustomURL.addEventListener('change', handleRadioChange);
-
-// destinationUrlInput.addEventListener('change', handleDestinationURLInputChange);
-
-// let changeColor = document.getElementById('changeColor');
-
-  // chrome.storage.sync.get('color', function(data) {
-  //   changeColor.style.backgroundColor = data.color;
-  //   changeColor.setAttribute('value', data.color);
-  // });
-
-  // changeColor.onclick = function(element) {
-  //   let color = element.target.value;
-  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //     chrome.tabs.executeScript(
-  //         tabs[0].id,
-  //         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  //   });
-  // };
-
-  // function post(path, params, method) {
-  //   method = method || "post"; // Set method to post by default if not specified.
-  
-  //   // The rest of this code assumes you are not using a library.
-  //   // It can be made less wordy if you use one.
-  //   var form = document.createElement("form");
-  //   form.setAttribute("method", method);
-  //   form.setAttribute("action", path);
-  
-  //   for(var key in params) {
-  //       if(params.hasOwnProperty(key)) {
-  //           var hiddenField = document.createElement("input");
-  //           hiddenField.setAttribute("type", "hidden");
-  //           hiddenField.setAttribute("name", key);
-  //           hiddenField.setAttribute("value", params[key]);
-  
-  //           form.appendChild(hiddenField);
-  //        }
-  //   }
-  
-  //   document.body.appendChild(form);
-  //   form.submit();
-  // }
-  // post('http://localhost:8083/planchoice/sso-login', {'SAMLResponse':''});
-
-
-  // let formElement = document.getElementById('sso-form');
-  // let tokenElement = document.getElementsByName('token');
-  // let targetUrlElement = document.getElementsByName('targetUrl');
-
-
-  // formElement.addEventListener("submit", function(e){
-  //   e.preventDefault();
-  //   let targetUrl = targetUrlElement[0].value;
-  //   let token = tokenElement[0].value;
-  //   formSubmitHandler(targetUrl, { 'SAMLResponse': token }, 'post');
-  // });
-
-  // function formSubmitHandler(path, params, method) {
-  //   method = method || "post"; // Set method to post by default if not specified.
-  //   let form = document.createElement("form");
-  //   form.setAttribute("method", method);
-  //   form.setAttribute("action", path);
-
-  //   for(var key in params) {
-  //       if(params.hasOwnProperty(key)) {
-  //           var hiddenField = document.createElement("input");
-  //           hiddenField.setAttribute("type", "hidden");
-  //           hiddenField.setAttribute("name", key);
-  //           hiddenField.setAttribute("value", params[key]);
-
-  //           form.appendChild(hiddenField);
-  //         }
-  //   }
-  //   document.body.appendChild(form);
-  //   form.submit();
-  // }
